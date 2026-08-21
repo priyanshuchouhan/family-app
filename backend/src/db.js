ad63f4d59b444dcfee88246d67b99c393a81ad3a
Priyanshu Chouhan <priyanshuchouhan179@gmail.com>
2026-08-21 15:21:59 +0530

    support database url and ci database

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,9 +1,17 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: false
-});
+const pool = process.env.DATABASE_URL
+    ? new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: false
+    })
+    : new Pool({
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+    });
 
 module.exports = pool;
